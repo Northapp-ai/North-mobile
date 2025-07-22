d9e32166d5c1d054626406b147752676429d72b6
Maicol Felipe Duque Urrea <34352054+MaicolDuque@users.noreply.github.com>
2025-07-21 22:54:23 -0500

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -33,7 +33,7 @@ export default function Index() {
           the age of distraction.
         </Text>
 
-        <Button redirecTo="/profile/profile" text="Get started" />
+        <Button redirecTo="/auth/login" text="Get started" />
       </View>
     </ScrollView>
   );
